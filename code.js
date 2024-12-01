8ac496993dc0bbdc124ca6026d18a8ad5e8c2282
JingtianWang-28 <144759925+JingtianWang-28@users.noreply.github.com>
2024-11-30 17:55:25 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,35 @@
 function augmentingPath(graph, start, end) {
-    return [];
+    if (start === end) {
+        return [start];
+    }
+
+    let queue = [start]; 
+    let visited = new Set(); 
+    let predecessors = {}; 
+    visited.add(start); 
+
+    while (queue.length > 0) {
+        let node = queue.shift(); 
+
+        
+        for (let neighbor in graph[node]) {
+            if (!visited.has(neighbor)) {
+                visited.add(neighbor); 
+                predecessors[neighbor] = node; 
+                queue.push(neighbor); 
+
+                if (neighbor === end) {
+                    let path = [end];
+                    let current = end;
+                    while (current !== start) {
+                        current = predecessors[current];
+                        path.unshift(current); 
+                    }
+                    return path;
+                }
+            }
+        }
+    }
+
+    return []; 
 }
